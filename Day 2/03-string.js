067ba8c87d9baa087727ca528cf8a9126e657ad8
Mijanur <onlineghor87@gmail.com>
2023-07-04 10:30:38 +0600

    Day 2 strings

---
+++ Day 2/03-string.js
@@ -0,0 +1,13 @@
+let text = "SDFKJSADFLKJSANDFSKJD";
+console.log(text.length);
+
+let chooping = "We are the so-called \"Vikings\" from the north.";
+let chooping2 = "We are the \tso-called \\Vikings\\ from \
+the north.";
+console.log(chooping);
+console.log(chooping2);
+
+let m = "Mijan";
+let n = new String("Mijan");
+
+console.log(m === n);
